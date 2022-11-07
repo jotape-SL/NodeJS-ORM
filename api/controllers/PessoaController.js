dd3b749919a3468842b59c23917d313e9f7a5f5c
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-11-07 15:30:43 -0300

    Finding all people on db

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,9 +1,9 @@
-const db = require('../models');
+const db = require("../models");
 
 class PessoaController {
   static async listarPessoas(req, res) {
     try {
-      const todasPessoas = await database.Pessoas.findAll();
+      const todasPessoas = await db.Pessoas.findAll();
       return res.status(200).json(todasPessoas);
     } catch (error) {
       return res.status(500).json(error.message);
